481a8c95468e3296d5d4d995329d7aa33e85f6db
Rakesh Mukundan <rakesh@rakeshmukundan.in>
2016-03-30 21:15:29 +0200

    Updated fab file to user local instead of run

--- fabfile.py
+++ fabfile.py
@@ -5,21 +5,21 @@ env.hosts = ['localhost']
 
 
 def remote_uname():
-    run('uname -a')
+    local('uname -a')
 
 def remote_hostname():
-    return run('hostname')
+    return local('hostname')
 
 def apt_get(*packages):
-    run('apt-get -y --no-upgrade install %s' % ' '.join(packages), shell=False)
+    local('apt-get -y --no-upgrade install %s' % ' '.join(packages), shell=False)
 
 def apt_get_update():
-    run('apt-get update')
+    local('apt-get update')
 
 
 def install_nginx():
     with settings(hide('warnings', 'stderr'), warn_only=True):
-        result = run('dpkg-query --show nginx')
+        result = local('dpkg-query --show nginx')
     if result.failed is False:
         warn('nginx is already installed')
         return    
@@ -27,7 +27,7 @@ def install_nginx():
 
 def install_mysql_server():
     with settings(hide('warnings', 'stderr'), warn_only=True):
-        result = run('dpkg-query --show mysql-server')
+        result = local('dpkg-query --show mysql-server')
     if result.failed is False:
         warn('mysql-server is already installed')
         return    
@@ -35,7 +35,7 @@ def install_mysql_server():
 
 def install_redis():
     with settings(hide('warnings', 'stderr'), warn_only=True):
-        result = run('dpkg-query --show redis-server')
+        result = local('dpkg-query --show redis-server')
     if result.failed is False:
         warn('redis-server is already installed')
         return    
@@ -43,7 +43,7 @@ def install_redis():
 
 def install_supervisor():
     with settings(hide('warnings', 'stderr'), warn_only=True):
-        result = run('dpkg-query --show supervisor')
+        result = local('dpkg-query --show supervisor')
     if result.failed is False:
         warn('supervisor is already installed')
         return    
@@ -51,67 +51,67 @@ def install_supervisor():
 
 
 def install_iptables_persistant():
-    run('DEBIAN_FRONTEND=noninteractive apt-get install iptables-persistent --yes --force-yes')
+    local('DEBIAN_FRONTEND=noninteractive apt-get install iptables-persistent --yes --force-yes')
 
 
 def install_unifispot():
-    run('apt-get install python-dev libmysqlclient-dev python-pip git bc nginx -y') 
-    run('pip install -U pip')    
+    local('apt-get install python-dev libmysqlclient-dev python-pip git bc nginx -y') 
+    local('pip install -U pip')    
     with cd("/usr/share/nginx/"):
-        run('git clone git@github.com:unifispot/poppet.git')
-        run('mkdir -p puppet/instance')
-        run('touch puppet/instance/__init__.py')
-        run(' cp instance_sample.py /usr/share/nginx/puppet/instance/config.py')
+        local('git clone git@github.com:unifispot/poppet.git')
+        local('mkdir -p puppet/instance')
+        local('touch puppet/instance/__init__.py')
+        local(' cp instance_sample.py /usr/share/nginx/puppet/instance/config.py')
     with cd("/usr/share/nginx/puppet"):
-        run('mkdir -p unifispot/static/uploads/')
-        run('pip install virtualenv')
-        run('virtualenv .env')
-        run('mkdir -p  logs')
+        local('mkdir -p unifispot/static/uploads/')
+        local('pip install virtualenv')
+        local('virtualenv .env')
+        local('mkdir -p  logs')
         with prefix('source .env/bin/activate'):          
-            run('pip install -r requirements.txt')        
+            local('pip install -r requirements.txt')        
     #configure uwsgi
-    run('mkdir -p /var/log/uwsgi')
-    run('chown -R www-data:www-data /var/log/uwsgi')   
-    run('mkdir -p /etc/uwsgi && run mkdir -p /etc/uwsgi/vassals')
-    run('rm -rf /etc/uwsgi/vassals/uwsgi.ini ')
-    run('ln -s /usr/share/nginx/puppet/uwsgi.ini /etc/uwsgi/vassals')
-    run('chown -R www-data:www-data /usr/share/nginx/puppet')
-    run('chown -R www-data:www-data /var/log/uwsgi/')
-    run('cp /usr/share/nginx/puppet/uwsgi.conf /etc/init/')
-    run('rm -rf /etc/nginx/sites-enabled/default')
+    local('mkdir -p /var/log/uwsgi')
+    local('chown -R www-data:www-data /var/log/uwsgi')   
+    local('mkdir -p /etc/uwsgi && local mkdir -p /etc/uwsgi/vassals')
+    local('rm -rf /etc/uwsgi/vassals/uwsgi.ini ')
+    local('ln -s /usr/share/nginx/puppet/uwsgi.ini /etc/uwsgi/vassals')
+    local('chown -R www-data:www-data /usr/share/nginx/puppet')
+    local('chown -R www-data:www-data /var/log/uwsgi/')
+    local('cp /usr/share/nginx/puppet/uwsgi.conf /etc/init/')
+    local('rm -rf /etc/nginx/sites-enabled/default')
     with cd('/etc/nginx/sites-available'):
-        run('cp wifiapp.conf /etc/nginx/sites-available/wifiapp.conf')
-        run('ln -s /etc/nginx/sites-available/wifiapp.conf /etc/nginx/sites-enabled/wifiapp')
-        run('rm /etc/nginx/sites-enabled/default')
+        local('cp wifiapp.conf /etc/nginx/sites-available/wifiapp.conf')
+        local('ln -s /etc/nginx/sites-available/wifiapp.conf /etc/nginx/sites-enabled/wifiapp')
+        local('rm /etc/nginx/sites-enabled/default')
 
-    run('service nginx restart')
-    run('service uwsgi restart')
+    local('service nginx restart')
+    local('service uwsgi restart')
 
 
 
 def configure_firewall_app(flush=True):
     internal_ips = droplets.get_all_private_ips()
     if flush:
-        run('iptables -F')    
-    run('iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT')
-    run('iptables -A INPUT -p tcp --dport 22 -j ACCEPT')
-    run('iptables -A INPUT -p tcp --dport 80 -j ACCEPT')
-    run('iptables -A INPUT -p tcp --dport 443 -j ACCEPT')
-    run('iptables -I INPUT 1 -i lo -j ACCEPT')
-    run('iptables -A INPUT -p tcp --dport 8081 -j ACCEPT')
-    run('iptables -A INPUT -p tcp --dport 8080 -j ACCEPT')
-    run('iptables -A INPUT -p tcp --dport 8443 -j ACCEPT')
-    run('iptables -A INPUT -p tcp --dport 8880 -j ACCEPT')
-    run('iptables -A INPUT -p tcp --dport 8843 -j ACCEPT')
-    run('iptables -A INPUT -p tcp --dport 27117 -j ACCEPT')  
-    run('iptables -A INPUT -j DROP')
-    run('invoke-rc.d iptables-persistent save')
+        local('iptables -F')    
+    local('iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT')
+    local('iptables -A INPUT -p tcp --dport 22 -j ACCEPT')
+    local('iptables -A INPUT -p tcp --dport 80 -j ACCEPT')
+    local('iptables -A INPUT -p tcp --dport 443 -j ACCEPT')
+    local('iptables -I INPUT 1 -i lo -j ACCEPT')
+    local('iptables -A INPUT -p tcp --dport 8081 -j ACCEPT')
+    local('iptables -A INPUT -p tcp --dport 8080 -j ACCEPT')
+    local('iptables -A INPUT -p tcp --dport 8443 -j ACCEPT')
+    local('iptables -A INPUT -p tcp --dport 8880 -j ACCEPT')
+    local('iptables -A INPUT -p tcp --dport 8843 -j ACCEPT')
+    local('iptables -A INPUT -p tcp --dport 27117 -j ACCEPT')  
+    local('iptables -A INPUT -j DROP')
+    local('invoke-rc.d iptables-persistent save')
  
 
 
 def configure_celery():
-    run('ln -s /usr/share/nginx/unifispot/supervisord.conf  /etc/supervisor/conf.d/unifispot_celery.conf')
-    run('service supervisor restart')
+    local('ln -s /usr/share/nginx/unifispot/supervisord.conf  /etc/supervisor/conf.d/unifispot_celery.conf')
+    local('service supervisor restart')
 
 def build_new_app_server():
     apt_get_update()
